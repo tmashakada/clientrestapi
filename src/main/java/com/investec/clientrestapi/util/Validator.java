bd830845e36cf1bd9ccac7dca8c60ae87d083894
Tafadzwa Mashakada <tmashakada10@gmail.com>
2023-09-04 11:56:19 +0200

    implementing TDD  unit test on the Validator

--- src/main/java/com/investec/clientrestapi/util/Validator.java
+++ src/main/java/com/investec/clientrestapi/util/Validator.java
@@ -1,14 +1,47 @@
 package com.investec.clientrestapi.util;
 
+import java.util.regex.Pattern;
+
 public class Validator {
+
     public static Boolean isIdNumberValid(String idNumber) {
         boolean isValid = false;
+        if (idNumber.length()!= 13 || !isNumeric(idNumber) ) {
+            return  isValid;
+        }
+        int sum = 0;
+        char[] idChars = idNumber.toCharArray();
+        StringBuilder even= new StringBuilder();
+        for (int i = 0; i <= idChars.length-2; i++) {
+            int digit = Character.getNumericValue(idChars[i]);
+            if(((i+1)%2)!=0){
+                sum+=digit;
+            }else{
+                even.append(idChars[i]);
+            }
+        }
+        int sumEven=getSum(Integer.parseInt(even.toString())*2);
+        int checkDigit=10-((sumEven+sum)%10);
+        if(checkDigit == Integer.parseInt(String.valueOf(idNumber.charAt(12)))){
+            isValid=true;
+        }
 
         return isValid;
     }
-    public static boolean isNumeric(String strNum) {
 
-        return false;
+    public static int getSum(int n) {
+        int sum;
+        for (sum = 0; n > 0; sum += n % 10, n /= 10);
+        return sum;
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null || strNum.isEmpty()) {
+            return false;
+        }
+
+       return strNum.chars().allMatch( Character::isDigit );
+
     }
 
 }
---
+++ src/test/java/com/investec/clientrestapi/util/ValidatorTest.java
@@ -0,0 +1,30 @@
+package com.investec.clientrestapi.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+    @Test
+    void givenIdNumber_IsIdNumberValid_ShouldReturnTrue(){
+       assertThat(Validator.isIdNumberValid("8605065397083")).isEqualTo(true);
+       assertThat(Validator.isIdNumberValid("860B065397083")).isEqualTo(false);
+       assertThat(Validator.isIdNumberValid("86050653970")).isEqualTo(false);
+    }
+
+    @Test
+    void givenAnInteger_SumTheDigits_ShouldReturnSum(){
+        int sum=Validator.getSum(374789);
+        assertThat(sum).isEqualTo(38);
+    }
+    @Test
+    void givenStringCheckWetherStringIsNumeric_ShouldReturnTrueOrFalse(){
+        boolean isNumeric=Validator.isNumeric("860B065397083");
+        boolean isNumeric2=Validator.isNumeric("6789");
+        boolean isNumeric3=Validator.isNumeric("");
+        assertThat(isNumeric).isEqualTo(false);
+        assertThat(isNumeric2).isEqualTo(true);
+        assertThat(isNumeric3).isEqualTo(false);
+    }
+}
